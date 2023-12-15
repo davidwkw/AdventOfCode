fe7245200d23c428b547d41b9d00e5761dcf0687
David Wang <devtesting404@gmail.com>
2023-12-15 16:08:31 +0800

    feat: add solution for part1 and part2

---
+++ 2023/day1/part1.py
@@ -0,0 +1,30 @@
+import sys
+import re
+
+if __name__ == "__main__":
+    if (len(sys.argv) != 2):
+        print("Invalid number of arguments. File path should follow script name. I.e. <day1>.py filename")
+
+    sum: int
+    filename: str
+
+    filename = sys.argv[1]
+    sum = 0
+    with open(filename, 'r') as file:
+        line: str
+
+        line = file.readline()
+        while (line):
+            sum_string: str
+            sum_string = ''
+            match = re.search(r"\d", line)
+            if match == None:
+                continue
+            sum_string += match.group()
+            line = line[::-1]
+            match = re.search(r"\d", line)
+            sum_string += match.group()
+            sum += int(sum_string)
+            line = file.readline()
+
+    print(sum)
---
+++ 2023/day1/part2.py
@@ -0,0 +1,38 @@
+import sys
+import re
+from collections.abc import Iterable
+
+number_strings: Iterable[str]
+pattern: str
+
+number_strings = "one two three four five six seven eight nine".split()
+pattern = "(?=(" + "|".join(number_strings) + "|\\d))"
+print(pattern)
+
+def f(x: str) -> str:
+    if x in number_strings:
+        return str(number_strings.index(x) + 1)
+    return x
+
+if __name__ == "__main__":
+    if (len(sys.argv) != 2):
+        print("Invalid number of arguments. File path should follow script name. I.e. <day1>.py filename")
+
+    sum: int
+    filename: str
+
+    filename = sys.argv[1]
+    sum = 0
+    with open(filename, 'r') as file:
+        line: str
+
+        line = file.readline()
+        while (line):
+            matches: Iterable[str]
+            matches = list(map(f, re.findall(pattern, line)))
+            if not matches:
+                continue
+            sum += int(matches[0] + matches[-1])
+            line = file.readline()
+
+    print(sum)
